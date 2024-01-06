e28caed75e162e1b9adadd7087b9a6f28c21f3e6
Toby Clemson <tobyclemson@gmail.com>
2024-01-06 15:56:13 +0100

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/vent "0.7.0-RC0"
+(defproject io.logicblocks/vent "0.7.0-RC1"
   :description "Rule based event processing engine."
   :url "https://github.com/logicblocks/vent"
 
